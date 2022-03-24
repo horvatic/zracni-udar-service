63c83d434d8b0c4e65224f9f463aa2042e812c6a
horvatic <sbhorvatic@gmail.com>
2022-03-24 18:36:07 +0000

    cloud migration to ECS

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,16 @@
 FROM golang:alpine AS builder
-ENV GOOS=linux \
-    GOARCH=arm64
+ENV GOOS=linux
 
 WORKDIR /build
 COPY . .
 RUN go build -o bin/zracni-udar-service cmd/zracni-udar-service/main.go
 
 FROM alpine:3
-ENV NAMESPACE=default \
-		SERVICE=default \
-        MONGO_CONNECTION_STRING=default \
+ENV MONGO_CONNECTION_STRING=default \
         MONGO_DATABASE=default \
         MONGO_COLLECTION=default \
-        FRONT_END_HOST=default
+        FRONT_END_HOST=default \
+        GITHUB_PAT=default
 
 RUN apk --no-cache add ca-certificates
 WORKDIR /dist
--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,94 +1,85 @@
 package routes
 
 import (
-	"fmt"
-
 	"github.com/gorilla/mux"
 
 	"github.com/horvatic/zracni-udar-service/pkg/controller"
 )
 
-func SetProjectRoutes(c controller.ProjectController, router *mux.Router, namespace string, serviceName string) *mux.Router {
-	router.HandleFunc(buildRoute(namespace, serviceName, "/projectsmetadata"), c.GetAllProjectsMetaData).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/projectmetadata"), c.CreateProject).Methods("POST")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/projectmetadata/{id}"), c.UpdateProject).Methods("PATCH")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{id}"), c.GetProjectMetaDataById).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{id}"), c.DeleteProject).Methods("DELETE")
+func SetProjectRoutes(c controller.ProjectController, router *mux.Router) *mux.Router {
+	router.HandleFunc("/projectsmetadata", c.GetAllProjectsMetaData).Methods("GET")
+	router.HandleFunc("/projectmetadata", c.CreateProject).Methods("POST")
+	router.HandleFunc("/projectmetadata/{id}", c.UpdateProject).Methods("PATCH")
+	router.HandleFunc("/project/{id}", c.GetProjectMetaDataById).Methods("GET")
+	router.HandleFunc("/project/{id}", c.DeleteProject).Methods("DELETE")
 	return router
 }
 
-func SetNoteRoutes(c controller.NoteController, router *mux.Router, namespace string, serviceName string) *mux.Router {
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{id}/notes"), c.GetNotesByProjectId).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/notes/{noteId}"), c.GetNote).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/notes"), c.CreateNote).Methods("POST")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/notes/{noteId}"), c.UpdateNote).Methods("PATCH")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/notes/{noteId}"), c.DeleteNote).Methods("DELETE")
+func SetNoteRoutes(c controller.NoteController, router *mux.Router) *mux.Router {
+	router.HandleFunc("/project/{id}/notes", c.GetNotesByProjectId).Methods("GET")
+	router.HandleFunc("/project/{projectId}/notes/{noteId}", c.GetNote).Methods("GET")
+	router.HandleFunc("/project/{projectId}/notes", c.CreateNote).Methods("POST")
+	router.HandleFunc("/project/{projectId}/notes/{noteId}", c.UpdateNote).Methods("PATCH")
+	router.HandleFunc("/project/{projectId}/notes/{noteId}", c.DeleteNote).Methods("DELETE")
 	return router
 }
 
-func SetBlogRoutes(c controller.BlogController, router *mux.Router, namespace string, serviceName string) *mux.Router {
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{id}/blogs"), c.GetBlogsByProjectId).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/blogs/{blogId}"), c.GetBlog).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/blogs"), c.CreateBlog).Methods("POST")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/blogs/{blogId}"), c.UpdateBlog).Methods("PATCH")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/blogs/{blogId}"), c.DeleteBlog).Methods("DELETE")
+func SetBlogRoutes(c controller.BlogController, router *mux.Router) *mux.Router {
+	router.HandleFunc("/project/{id}/blogs", c.GetBlogsByProjectId).Methods("GET")
+	router.HandleFunc("/project/{projectId}/blogs/{blogId}", c.GetBlog).Methods("GET")
+	router.HandleFunc("/project/{projectId}/blogs", c.CreateBlog).Methods("POST")
+	router.HandleFunc("/project/{projectId}/blogs/{blogId}", c.UpdateBlog).Methods("PATCH")
+	router.HandleFunc("/project/{projectId}/blogs/{blogId}", c.DeleteBlog).Methods("DELETE")
 	return router
 }
 
-func SetServiceRoutes(c controller.ServiceInfoController, router *mux.Router, namespace string, serviceName string) *mux.Router {
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{id}/services"), c.GetServicesInfoByProjectId).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/services/{serviceId}"), c.GetServiceInfo).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/services"), c.CreateServiceInfo).Methods("POST")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/services/{serviceId}"), c.UpdateServiceInfo).Methods("PATCH")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/services/{serviceId}"), c.DeleteServiceInfo).Methods("DELETE")
+func SetServiceRoutes(c controller.ServiceInfoController, router *mux.Router) *mux.Router {
+	router.HandleFunc("/project/{id}/services", c.GetServicesInfoByProjectId).Methods("GET")
+	router.HandleFunc("/project/{projectId}/services/{serviceId}", c.GetServiceInfo).Methods("GET")
+	router.HandleFunc("/project/{projectId}/services", c.CreateServiceInfo).Methods("POST")
+	router.HandleFunc("/project/{projectId}/services/{serviceId}", c.UpdateServiceInfo).Methods("PATCH")
+	router.HandleFunc("/project/{projectId}/services/{serviceId}", c.DeleteServiceInfo).Methods("DELETE")
 	return router
 }
 
-func SetVideoRoutes(c controller.VideoController, router *mux.Router, namespace string, serviceName string) *mux.Router {
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{id}/videos"), c.GetVideosByProjectId).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/videos/{videoId}"), c.GetVideo).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/videos"), c.CreateVideo).Methods("POST")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/videos/{videoId}"), c.UpdateVideo).Methods("PATCH")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/videos/{videoId}"), c.DeleteVideo).Methods("DELETE")
+func SetVideoRoutes(c controller.VideoController, router *mux.Router) *mux.Router {
+	router.HandleFunc("/project/{id}/videos", c.GetVideosByProjectId).Methods("GET")
+	router.HandleFunc("/project/{projectId}/videos/{videoId}", c.GetVideo).Methods("GET")
+	router.HandleFunc("/project/{projectId}/videos", c.CreateVideo).Methods("POST")
+	router.HandleFunc("/project/{projectId}/videos/{videoId}", c.UpdateVideo).Methods("PATCH")
+	router.HandleFunc("/project/{projectId}/videos/{videoId}", c.DeleteVideo).Methods("DELETE")
 	return router
 }
 
-func SetDiagramRoutes(c controller.DiagramController, router *mux.Router, namespace string, serviceName string) *mux.Router {
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{id}/diagrams"), c.GetDiagramsByProjectId).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/diagrams/{diagramId}"), c.GetDiagram).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/diagrams"), c.CreateDiagram).Methods("POST")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/diagrams/{diagramId}"), c.UpdateDiagram).Methods("PATCH")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/diagrams/{diagramId}"), c.DeleteDiagram).Methods("DELETE")
+func SetDiagramRoutes(c controller.DiagramController, router *mux.Router) *mux.Router {
+	router.HandleFunc("/project/{id}/diagrams", c.GetDiagramsByProjectId).Methods("GET")
+	router.HandleFunc("/project/{projectId}/diagrams/{diagramId}", c.GetDiagram).Methods("GET")
+	router.HandleFunc("/project/{projectId}/diagrams", c.CreateDiagram).Methods("POST")
+	router.HandleFunc("/project/{projectId}/diagrams/{diagramId}", c.UpdateDiagram).Methods("PATCH")
+	router.HandleFunc("/project/{projectId}/diagrams/{diagramId}", c.DeleteDiagram).Methods("DELETE")
 	return router
 }
 
-func SetGitRepoRoutes(c controller.GitRepoController, router *mux.Router, namespace string, serviceName string) *mux.Router {
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{id}/gitrepos"), c.GetGitReposByProjectId).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/gitrepos/{gitRepoId}"), c.GetGitRepo).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/gitrepos"), c.CreateGitRepo).Methods("POST")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/gitrepos/{gitRepoId}"), c.UpdateGitRepo).Methods("PATCH")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/gitrepos/{gitRepoId}"), c.DeleteGitRepo).Methods("DELETE")
+func SetGitRepoRoutes(c controller.GitRepoController, router *mux.Router) *mux.Router {
+	router.HandleFunc("/project/{id}/gitrepos", c.GetGitReposByProjectId).Methods("GET")
+	router.HandleFunc("/project/{projectId}/gitrepos/{gitRepoId}", c.GetGitRepo).Methods("GET")
+	router.HandleFunc("/project/{projectId}/gitrepos", c.CreateGitRepo).Methods("POST")
+	router.HandleFunc("/project/{projectId}/gitrepos/{gitRepoId}", c.UpdateGitRepo).Methods("PATCH")
+	router.HandleFunc("/project/{projectId}/gitrepos/{gitRepoId}", c.DeleteGitRepo).Methods("DELETE")
 	return router
 }
 
-func SetProjectBuildRoutes(c controller.ProjectBuildController, router *mux.Router, namespace string, serviceName string) *mux.Router {
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{id}/buildmetadatas"), c.GetBuildMetaDatasByProjectId).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/buildmetadatas/{buildId}"), c.GetBuildMetaData).Methods("GET")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/buildmetadatas"), c.CreateBuildMetaData).Methods("POST")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/buildmetadatas/{buildId}"), c.UpdateBuildMetaData).Methods("PATCH")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/buildmetadatas/{buildId}"), c.DeleteBuildMetaData).Methods("DELETE")
-	router.HandleFunc(buildRoute(namespace, serviceName, "/project/{projectId}/{buildId}/builds"), c.GetBuildsForProject).Methods("GET")
+func SetProjectBuildRoutes(c controller.ProjectBuildController, router *mux.Router) *mux.Router {
+	router.HandleFunc("/project/{id}/buildmetadatas", c.GetBuildMetaDatasByProjectId).Methods("GET")
+	router.HandleFunc("/project/{projectId}/buildmetadatas/{buildId}", c.GetBuildMetaData).Methods("GET")
+	router.HandleFunc("/project/{projectId}/buildmetadatas", c.CreateBuildMetaData).Methods("POST")
+	router.HandleFunc("/project/{projectId}/buildmetadatas/{buildId}", c.UpdateBuildMetaData).Methods("PATCH")
+	router.HandleFunc("/project/{projectId}/buildmetadatas/{buildId}", c.DeleteBuildMetaData).Methods("DELETE")
+	router.HandleFunc("/project/{projectId}/{buildId}/builds", c.GetBuildsForProject).Methods("GET")
 	return router
 }
 
-func SetHeathRoutes(router *mux.Router, namespace string, serviceName string) *mux.Router {
-	router.HandleFunc(buildRoute(namespace, serviceName, "/health"), controller.Health).Methods("GET")
+func SetHeathRoutes(router *mux.Router) *mux.Router {
+	router.HandleFunc("/health", controller.Health).Methods("GET")
 	return router
-}
-
-func buildRoute(namespace string, serviceName string, baseRoute string) string {
-	if namespace == "" || serviceName == "" {
-		return baseRoute
-	}
-	return fmt.Sprintf("/%s/%s%s", namespace, serviceName, baseRoute)
 }
